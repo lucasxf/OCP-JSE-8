107a28a0804523213af092513fd138309ddf01d5
lucasxf <lucasxferreira@gmail.com>
2018-04-10 21:48:46 -0300

    Question 4, chapter 1, correct. 4/20

---
+++ book/c1_enums_&_nested_classes/test/BaseballTeam.java
@@ -0,0 +1,24 @@
+// Chapter 1 - Question 4
+// My answer: B
+// Correct answer: B
+public class BaseballTeam {
+    
+    private String city, mascot;
+    private int numberOfPlayers;
+
+    @Override
+    public boolean equals( Object obj) {
+        if ( !(obj instanceof BaseballTeam)) {
+            return false;
+        }
+        BaseballTeam other = (BaseballTeam) obj;
+
+        return ( city.equals(other.city) && mascot.equals(other.mascot));
+    }
+
+    @Override
+    public int hashCode() {
+        return numberOfPlayers;
+    }
+
+}
