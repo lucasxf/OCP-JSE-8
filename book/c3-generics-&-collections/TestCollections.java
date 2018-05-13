985091974ccb9916d7d109737ccd8fb63da4bf26
lucasxf <lucasxferreira@gmail.com>
2018-05-13 09:42:36 -0300

    Created folder and file for chapter 3 - generics and collections

---
+++ book/c3-generics-&-collections/TestCollections.java
@@ -0,0 +1,15 @@
+public class TestCollections {
+
+    public static void main (String[] args) {
+        TestCollections test = new TestCollections();
+
+    }
+
+    void testArrays() {
+
+    }
+
+    void testLists() {
+        
+    }
+}
