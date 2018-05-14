d22e3990489caabbdfc244cf170d02ac2f8dc47d
lucasxf <lucasxferreira@gmail.com>
2018-05-14 19:54:12 -0300

    Testing upper bounded wildcards

--- book/c3-generics-&-collections/TestingBounds.java
+++ book/c3-generics-&-collections/TestingBounds.java
@@ -1,7 +1,57 @@
-class TestingBounds {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Car {  }
+
+class Ferrari extends Car { 
+    @Override
+    public String toString() {
+        return "Ferrari Modena";
+    }
+}
+class Porsche extends Car {
+    @Override
+    public String toString() {
+        return "Porsche Cayenne S";
+    }
+}
+public class TestingBounds {
 
     public static void main( String[] args) {
-        
+        TestingBounds testingBounds = new TestingBounds();
+        testingBounds.testArraysNLists();
+        testingBounds.testUpperBounds();
     }
-    
+
+    void testUpperBounds() {
+        // won't compile, incompatible types.
+        // List<Car> cars = new ArrayList<Ferrari>();
+        List<? extends Car> cars = new ArrayList<>(); // [Ferrari Modena]
+        // even though both Ferrari and Porsche are Car subclasses, 
+        // this won't compile because upper bounded or unbounded wildcards
+        // makes the list logically immutable, so it cannot "grow".
+        // cars.add(new Porsche());
+        // cars.add(new Ferrari());
+
+        // works just fine and prints: "[Porsche Cayenne S]"
+        cars = Arrays.asList(new Porsche());
+        System.out.println(cars);
+    }
+    void testArraysNLists() {
+        // doesn't compile, because otherwise we would be able to:
+        // List<Object> l = new List<Integer>();
+        // l.add(1);
+        // List<String> ls = new List<>();
+        // l = ls;
+        // ls.add("crazy casting");
+        // List<Object> crazyNumbers = new List<Integer>();
+
+        // compiles, but throws runtime exceptions:
+        Object[] numbers = new Integer[3]; // ugly, but ok
+        Integer[] inteiros = { 1, 2, 3}; // reglular array
+        numbers = inteiros; // also ok
+        // throws ArrayStoreException
+        // numbers[0] = "123"; // not ok
+    }
 }
