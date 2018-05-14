5cddb37fab19c5c729a89301481b371a016cf5c2
lucasxf <lucasxferreira@gmail.com>
2018-05-14 18:38:46 -0300

    Still playing with arrays and lists

--- book/c3-generics-&-collections/TestCollections.java
+++ book/c3-generics-&-collections/TestCollections.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -43,7 +44,29 @@ public class TestCollections {
         // because Arrays.asList() returns a fixed-size list.
         // nameList.remove(1); // index overload
         // nameList.remove("Lucas"); // object overload
-
+        // but both will throw an "UnsupportedOperationException"
+        // nameList.add(0, "Skywalker");
+        // nameList.add("Luke");
+        nameList.set(0, "Skywalker"); // [Skywalker, Lucas Xavier]
+        
+        // both will print: "[Skywalker, Lucas Xavier]"
+        // because updating the nameList, will also
+        // update the array it is backed by:
+        System.out.println(nameList); // 
+        for ( String name : nameArray) {
+            System.out.print(String.format("%s, ", name));
+        }
+        System.out.println("");
+        int[] numbers = { 2, 8, 4, 6, 10 };
+        List<Integer> numberList = Arrays.asList(3, 9, 1, 5, 7);
+        Arrays.sort(numbers);
+        Collections.sort(numberList);
+        // prints: 2, 4, 6, 8, 10
+        for ( int number : numbers) {
+            System.out.print(number + ", ");
+        }
+        // prints: [1, 3, 5, 7, 9]
+        System.out.println("\n" + numberList);
     }
 
     void testLists() {
