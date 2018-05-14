0be60fdc8ac0747dd85f70f378fc3172e5ec76c5
lucasxf <lucasxferreira@gmail.com>
2018-05-14 20:07:59 -0300

    Still working on upper bounds

--- book/c3-generics-&-collections/TestingBounds.java
+++ book/c3-generics-&-collections/TestingBounds.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 class Car {  }
 
-class Ferrari extends Car { 
+class Ferrari extends Car {
+    
     @Override
     public String toString() {
         return "Ferrari Modena";
@@ -30,14 +31,47 @@ public class TestingBounds {
         List<? extends Car> cars = new ArrayList<>(); // [Ferrari Modena]
         // even though both Ferrari and Porsche are Car subclasses, 
         // this won't compile because upper bounded or unbounded wildcards
-        // makes the list logically immutable, so it cannot "grow".
+        // makes the list logically immutable, so it cannot "grow", it 
+        // could "shrink", however, had it had any elements.
         // cars.add(new Porsche());
         // cars.add(new Ferrari());
 
-        // works just fine and prints: "[Porsche Cayenne S]"
-        cars = Arrays.asList(new Porsche());
+        // works just fine and prints: "[Porsche Cayenne S, Ferrari Modena, Porsche Cayenne S]"
+        cars = Arrays.asList(new Porsche(), new Ferrari(), new Porsche());
         System.out.println(cars);
+
+        // false because it's trying to remove a new Porsche object
+        // not the very same within the list
+        boolean canRemoveAPorsche = cars.remove(new Porsche());
+        System.out.println(canRemoveAPorsche); // false
+        // throws UnsupportedOperationException
+        // because Arrays.asList returns immutable list
+        // canRemoveAPorsche = (cars.remove(0) instanceof Porsche);
+        System.out.println(cars); // "[Ferrari Modena, Porsche Cayenne S]"
+
+        List<Car> carros = new ArrayList<>();
+        carros.add(new Porsche());
+        carros.add(new Ferrari());
+        carros.add(new Porsche());
+        carros.add(new Ferrari());
+
+        List<? extends Car> garagem = carros;
+        List<? extends Car> garage = new ArrayList<>(carros);
+        System.out.println(garagem);
+        System.out.println(garage);
+
+        // works just fine, an upper-bounded list can lose elements
+        garage.remove(0);
+        System.out.println("After removing:");
+        System.out.println(garage); // [Porsche, Ferrari, Porsche]
+        System.out.println(carros);  // [Porsche, Ferrari, Porsche, Ferrari] 
+        System.out.println(garagem); // [Porsche, Ferrari, Porsche, Ferrari] 
+
+        // compiler error, upper-bounded lists cannot grow.
+        // garage.add(new Porsche());
+        // garage.add(new Porsche());
     }
+
     void testArraysNLists() {
         // doesn't compile, because otherwise we would be able to:
         // List<Object> l = new List<Integer>();
